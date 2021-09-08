cc0ecf92901ee3f87acfd84879db7c947bbc71e0
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-09-08 17:31:37 -0600

    display grid

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const StartBtn = document.querySelector('#start-button')
   const width = 10
   let timerId
+  let nextRandom = 0
 
   //The Tetrominoes
 
@@ -96,10 +97,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominos.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominos.length)
       current = theTetrominos[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()      
     }
   }
 
@@ -143,5 +146,29 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  //show up-next tetromino in mini grid display
+  const displaySquares = document.querySelector('.mini-grid div ')
+  const displayWidth = 4
+  let displayIndex = 0
+
+  //the tetrominos without rotations
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], // lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], // zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], // tTetromino
+    [0, 1, displayWidth, displayWidth+1], // oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], // iTetromino
+  ]
+
+  //display the shape in the mini-grid display
+  function displayShape() {
+    // remove any trace of a tetromino from the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
   
 })
